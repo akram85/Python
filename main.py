eeac46ed2d1efaf56b61c302b59a72181136d194
Akaram Ali <>
2022-10-01 09:14:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+l=[10,20,30]
+print(l)
+print(l[0])
+print(type(l[0]))
+
+a=True
+print(a)
+print(type(a))
+a=str(9)
+print(a,type(a))
+b=int(9.3)
+print(b,type(b))
+
+x=bool(3)
+y=bool(0)
+print(x,type(x))
+print(y,type(y))
+
+z=bool('akram')
+i=bool("")
+print(z,type(z))
+print(i,type(i))
+
+print(7//2)
+print(3**3)
+
+print(True or False)
+print(False or False)
+print (not False)
+s='coffee'
+print(s[0:5])
